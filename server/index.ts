5c2176fa84748c376f918e4eabe59147467f3e3c
robertkale20 <36280865-robertkale20@users.noreply.replit.com>
2025-03-12 14:08:06 +0000

    Restored to '2c294d283901f879a6c2d8f3eb83fac157be4adc'
    Replit-Restored-To: 2c294d283901f879a6c2d8f3eb83fac157be4adc

--- server/index.ts
+++ server/index.ts
@@ -3,9 +3,8 @@ import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
-// Increase body-parser limit to 50MB
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   const start = Date.now();
